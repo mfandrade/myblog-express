07beb2ff1c39916aa1bf88f1a677de9249d0f6c4
Marcelo F Andrade <mfandrade@gmail.com>
2023-05-20 09:33:31 -0300

    -ambasic posts route

--- server.js
+++ server.js
@@ -1,7 +1,10 @@
 const express = require('express')
+const routes_posts = require('./routes/posts')
 const app = express()
 
 app.set('view engine', 'ejs')
+
+app.use('/posts/', routes_posts)
 
 app.get('/', (req, res) => {
     res.render('default')
