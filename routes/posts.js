dfd1c0334b767271b4f5e482661604742980dcad
Marcelo F Andrade <mfandrade@gmail.com>
2023-05-21 13:51:20 -0300

    fix reassigning a const

--- routes/posts.js
+++ routes/posts.js
@@ -6,17 +6,17 @@ router.get('/new', (req, res) => {
     res.render('posts/new', { post: new Post() })
 })
 router.get('/:id', (req, res) => {
+    res.send(req.params.id)
 })
 
 router.post('/', async (req, res) => {
-    const post = new Post({
+    let post = new Post({
         title: req.body.title,
         content: req.body.content
     })
     try {
         post = await post.save()
-        //res.redirect(`/posts/${post.id}`)
-        res.redirect('/')
+        res.redirect(`/posts/${post.id}`)
     } catch (e) {
         res.render('posts/new', { post: post })
     }
