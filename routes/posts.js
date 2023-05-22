02ba85e92ef5ac3c120c1c6f231f0cfd04ddcb8d
Marcelo F Andrade <mfandrade@gmail.com>
2023-05-22 14:08:12 -0300

    refactors save for edit and new

--- routes/posts.js
+++ routes/posts.js
@@ -12,22 +12,44 @@ router.get('/:id', async (req, res) => {
     res.render('posts/view', { post: post })
 })
 
-router.post('/', async (req, res) => {
-    let post = new Post({
-        title: req.body.title,
-        content: req.body.content
-    })
-    try {
-        post = await post.save()
-        res.redirect(`/posts/${post.id}`)
-    } catch (e) {
-        res.render('posts/new', { post: post })
-    }
-})
+router.post('/', async (req, res, next) => {
+    req.post = new Post()
+    next()
+}, saveFrom('new'))
 
 router.delete('/:id', async (req, res) => {
     await Post.findByIdAndDelete(req.params.id)
     res.redirect('/')
 })
+
+router.get('/edit/:id', async (req, res) => {
+    const post = await Post.findById(req.params.id)
+    res.render('posts/edit', { post: post })
+})
+
+router.put('/:id', async (req, res, next) => {
+    req.post = await Post.findById(req.params.id)
+    next()
+}, saveFrom('edit'))
+
+function saveFrom(action) { // action = (new|edit)
+    return async (req, res) => {
+        let post = req.post
+        post.title = req.body.title
+        post.content = req.body.content
+
+        try {
+            post = await post.save()
+            res.redirect(`/posts/${post.id}`)
+
+        } catch (e) {
+            action = (action == 'new' || action == 'edit')
+                ? action
+                : 'new'
+
+            res.render(`posts/${action}`, { post: post })
+        }
+    }
+}
 
 module.exports = router
